8e05870547f5083d7b0add82a4e649cb70733972
ANUPAMCHAUDHARY1117 <anupam1996@gmail.com>
2020-05-14 14:52:25 +0530

    Adding Text Area Component

---
+++ src/components/text-area/text-area.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TextArea = () => {
+	return (
+		<div>
+			<label className='active'>Textarea</label>
+			<textarea id='textarea1' class='materialize-textarea' />
+		</div>
+	);
+};
+
+export default TextArea;
